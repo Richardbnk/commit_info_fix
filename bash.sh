4b1268dcf49294c465d68fe8e3b1918f30a01b00
Richard Banak <45636763+Richardbnk@users.noreply.github.com>
2025-01-15 21:09:43 -0300

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+OLD_EMAIL="old_email_to_replace@gmail.com"
+CORRECT_NAME="New Name label"
+CORRECT_EMAIL="new_email@gmail.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
